c12cbf174fe2466ae5b5213560ec829f8d60cd73
Nellie <tene23kl@student.ju.se>
2024-04-27 16:20:56 +0200

    images, screens

---
+++ .github/game.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(800, 500);
+  background(250, 250, 200);
+}
+//----Screens
+//Start screen
+function startScreen() {}
+
+function gameScreen() {}
+
+//win screen
+function winScreen() {}
+
+//lose screen
+function loseScreen() {}
+
+//----End of all code for screens
