b2d7e3939d49e6a7d1e8aecb411bee9a60936814
Vilmah22 <havi22tu@student.ju.se>
2024-05-02 15:54:36 +0200

    started on screens

---
+++ main.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas();
+}
+
+function startScreen() {
+  if (state === "start") {
+  }
+}
+
+function gameScreen() {
+  if (state === "game") {
+  }
+}
+
+function loseScreen() {
+  if (state === "lose") {
+  }
+}
+
+function winScreen() {
+  if (state === "win") {
+  }
+}
+
+function draw() {
+  if (state === "start") {
+    startScreen();
+  } else if (state === "game") {
+    gameScreen();
+  } else if (state === "lose") {
+    loseScreen();
+  } else if (state === "win") winScreen();
+}
