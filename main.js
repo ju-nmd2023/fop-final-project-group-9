b1f93cad2a61635b3bddc60109ec1e95ba002f94
Nellie <tene23kl@student.ju.se>
2024-05-06 16:25:41 +0200

    mandag
    start button

--- main.js
+++ main.js
@@ -3,7 +3,28 @@ function setup() {
 }
 
 function preload() {
-  startImg = loadImage("images/startscreenn.png");
+  startImg = loadImage("images/startscreenn.jpg");
+}
+
+function startButton() {
+  push();
+  let x = 200;
+  let y = 70;
+
+  fill(0);
+  stroke(247, 247, 5);
+  strokeWeight(5);
+  rect(410, 350, x, y);
+  textSize(20);
+  text("Start Game", 460, 390);
+
+  pop();
+}
+
+function mouseClicked() {
+  if (mouseX > 400 && mouseX < 600 && mouseY > 200 && mouseY < 400) {
+    gameScreen();
+  }
 }
 
 let state = "start";
@@ -34,6 +55,7 @@ function draw() {
   if (state === "start") {
     image(startImg, 0, 0);
     startScreen();
+    startButton();
   } else if (state === "game") {
     gameScreen();
   } else if (state === "lose") {
