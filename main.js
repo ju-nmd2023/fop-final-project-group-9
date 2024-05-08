9642b7a51638c1a819cbdedae056102d0db4ea35
Nellie <tene23kl@student.ju.se>
2024-05-08 15:17:15 +0200

    game
    charachters and hilife

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ function setup() {
   createCanvas(1000, 800);
 }
 
+let state = "start";
+let gameIsRunning = false;
+
 function preload() {
+  //all images
   startImg = loadImage("images/startscreenn.jpg");
+  akaImg = loadImage("images/akascreen.jpg");
+  loseImg = loadImage("images/youlosescrenn.jpg");
+  winImg = loadImage("images/winscreen.png");
+  hilifeImg = loadImage("images/hilife.png");
+  character2 = loadImage("images/character2.png");
+  character3 = loadImage("images/character3.png");
+  character4 = loadImage("images/character4.png");
+  character5 = loadImage("images/character5.png");
 }
 
 function startButton() {
@@ -22,14 +34,39 @@ function startButton() {
 }
 
 function mouseClicked() {
-  if (mouseX > 400 && mouseX < 600 && mouseY > 200 && mouseY < 400) {
-    gameScreen();
+  if (state === "start") {
+    if (
+      mouseX > 410 &&
+      mouseX < 410 + 200 &&
+      mouseY > 350 &&
+      mouseY < 350 + 70
+    ) {
+      state = "game";
+      // if (mouseX > 400 && mouseX < 600 && mouseY > 200 && mouseY < 400) {
+      //state = "game";
+    }
   }
 }
 
-let state = "start";
-let gameIsRunning = false;
+//functions for icons and characters
+let x = 600;
+function hiLife() {
+  image(hilifeImg, x, 50, 70, 140);
 
+  /*   if (x < 830) {
+    x = x + 2;
+  } else {
+    x = x - 2;
+  } */
+
+  if (x < 830) {
+    x = x + 2;
+  } else if (x > 830) {
+    x = x - 2;
+  }
+}
+
+//screens
 function startScreen() {
   if (state === "start") {
     image(startImg, 0, 0);
@@ -38,22 +75,26 @@ function startScreen() {
 
 function gameScreen() {
   if (state === "game") {
+    image(akaImg, 0, 0);
+    // image(hilifeImg, 600, 50, 70, 140);
+    hiLife();
   }
 }
 
 function loseScreen() {
   if (state === "lose") {
+    image(loseImg, 0, 0);
   }
 }
 
 function winScreen() {
   if (state === "win") {
+    image(winImg, 0, 0);
   }
 }
 
 function draw() {
   if (state === "start") {
-    image(startImg, 0, 0);
     startScreen();
     startButton();
   } else if (state === "game") {
